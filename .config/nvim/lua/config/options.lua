2ae49cd7b3030fb2866061c4d9b4a5d5637365e5
saurav-moktan <saurav.stamang@gmail.com>
2024-12-25 19:30:13 +0545

    feat: Update LazyVim options - Added and removed configurations

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,15 +6,12 @@
 --  ╚═════╝ ╚═╝        ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
 
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-vim.opt.number = true
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.showcmd = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.wrap = false
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.wildignore:append({ "*/node_modules/*" })
+
+local opt = vim.opt
+
+opt.smartindent = true -- Insert indents automatically
+opt.linebreak = true -- Wrap lines at convenient points
+opt.wrap = true -- Enable line wrap
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.smoothscroll = true --Enable smooth scrolling
+opt.wildmode = "longest:full,full" -- Command-line completion mode
